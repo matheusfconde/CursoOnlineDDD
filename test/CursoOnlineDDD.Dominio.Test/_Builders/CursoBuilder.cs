ea0b9a2a248f7c36256ce1faf2b2737509bdec16
matheusfconde <matheusf.conde@gmail.com>
2024-02-06 18:53:09 -0300

    Classe Curso Refatorada, sendo carregada para o projeto da aplicação

--- test/CursoOnlineDDD.Dominio.Test/_Builders/CursoBuilder.cs
+++ test/CursoOnlineDDD.Dominio.Test/_Builders/CursoBuilder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CursoOnlineDDD.Dominio.Test.Cursos;
+﻿using CursoOnlineTDD.Dominio.Cursos;
 
 namespace CursoOnlineDDD.Dominio.Test._Builders
 {
